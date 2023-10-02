acfbed14c3454211e3ff468dfdb8e6755bedc341
Mohab Saber <mohabsaber58@gmail.com>
2023-10-02 21:58:39 +0300

    Intial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 80 || 443;
+const cors = require('cors');
+
+
+async function fileReader(url){
+    try{
+    return await fs.promises.readFile(url, 'utf-8');
+
+    }catch(err){
+        return null
+    }
+}
+
+
+
+app.use(express.urlencoded({ extended:false }));
+app.use(cors())
+
+app.use(express.static(path.join(__dirname,'public')));
+app.listen(PORT,()=>{console.log(`Server is running on PORT ${PORT}`)});
