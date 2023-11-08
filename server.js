1c058157ba076b2766febc1c56a9f220f96e9907
Mohab Saber <mohabsaber58@gmail.com>
2023-11-08 22:09:21 +0200

    TELEGRAM BOT

--- server.js
+++ server.js
@@ -5,20 +5,14 @@ const token = process.env.TOKEN;
 
 const bot = new TelegramBots(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-  
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-  
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-  });
-bot.on('message', (msg) => {
-const chatId = msg.chat.id;
 
-// send a message to the chat acknowledging receipt of their message
-bot.sendMessage(chatId, msg);
+bot.on('message', (msg) => {
+
+const chatId = msg.chat.id;
+if(msg.text == 'كسم السيسي'){
+    bot.sendMessage(chatId, 'كسمين أمه')
+}else{
+
+    bot.sendMessage(chatId, 'كسم السيسي');
+}
 });
